09aebd19796cbd53f38d6598cc74c5f0cc3d551e
Osni Carfi <osnicarfi@hotmail.com>
2015-08-17 18:02:28 -0300

    Add devise User

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Remember to update after creating a Heroku
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
